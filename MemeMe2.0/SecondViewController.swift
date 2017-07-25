eb7a049138ee8b19b19ee3173dd382fe24b46211
Taylor Masterson <taylormasterson@Taylors-MacBook-Pro.local>
2017-07-25 06:11:09 -0500

    Initial Commit

---
+++ MemeMe2.0/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MemeMe2.0
+//
+//  Created by Taylor Masterson on 7/25/17.
+//  Copyright © 2017 Taylor Masterson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
